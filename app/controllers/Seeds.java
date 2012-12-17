c5a73eae5a5cb04cd408ca080d61a6a5238d5f88
Rainer Simon <rainer.simon@ait.ac.at>
2012-12-17 17:37:32 +0100

    Starting dashboard-style layout for timeline data

--- app/controllers/Seeds.java
+++ app/controllers/Seeds.java
@@ -1,12 +1,16 @@
 package controllers;
 
+import global.Global;
 import play.mvc.Controller;
 import play.mvc.Result;
+import uk.bl.monitrix.CrawlStatistics;
 
 public class Seeds extends Controller {
 	
+	private static CrawlStatistics stats = Global.getCrawlStatistics();
+	
 	public static Result index() {
-		return ok(views.html.seeds.index.render());
+		return ok(views.html.seeds.index.render(stats));
 	}
 	
 }

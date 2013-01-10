3c22bb2e08c9b21ec54ccc24d128884aa08345fe
Rainer Simon <rainer.simon@ait.ac.at>
2013-01-10 09:31:42 +0100

    Re-enabled basic alert view after refactoring

--- app/controllers/Alerts.java
+++ app/controllers/Alerts.java
@@ -1,14 +1,28 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import play.mvc.Controller;
 import play.mvc.Result;
+import uk.bl.monitrix.Global;
+import uk.bl.monitrix.model.Alert;
+import uk.bl.monitrix.model.AlertLog;
 
 public class Alerts extends Controller {
 	
-	// private static AlertLog alertLog = Global.getBackend().getAlertLog();
+	private static AlertLog alertLog = Global.getBackend().getAlertLog();
 	
 	public static Result index() {
-		return ok(views.html.alerts.index.render()); // alertLog.groupedByHost()));
+		// TODO improve
+		Iterator<Alert> it = alertLog.listAll();
+		
+		List<Alert> alerts = new ArrayList<Alert>();
+		while (it.hasNext())
+			alerts.add(it.next());
+		
+		return ok(views.html.alerts.index.render(alerts));
 	}
 	
 }

e1aa7198481fee662054357bbcdf8d6c399a4703
Rainer Simon <rainer.simon@ait.ac.at>
2013-03-21 15:27:58 +0100

    AJAX loading for compressability histogram

--- app/controllers/URLs.java
+++ app/controllers/URLs.java
@@ -1,14 +1,16 @@
 package controllers;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.List;
 
-import play.mvc.Controller;
+import play.libs.Json;
 import play.mvc.Result;
 import uk.bl.monitrix.Global;
 import uk.bl.monitrix.model.CrawlLog;
 import uk.bl.monitrix.model.CrawlLogEntry;
 
-public class URLs extends Controller {
+public class URLs extends AbstractController {
 	
 	private static CrawlLog crawlLog  = Global.getBackend().getCrawlLog();
 	
@@ -21,6 +23,18 @@ public class URLs extends Controller {
 		return ok(views.html.urls.urlInfo.render(entries));
 	}
 	
-	// TODO JSON output for this!
+	public static Result getCompressability() {
+		int intervals = getQueryParamAsInt("intervals", 50);
+		double increment = 1.0 / intervals;
+		
+		List<Point2D> histogram = new ArrayList<Point2D>();
+		for (int i=0; i<intervals; i++) {
+			double from = i * increment;
+			double to = from + increment;
+			histogram.add(new Point2D.Double(from, crawlLog.searchByCompressability(from, to, 0, 0).totalResults()));
+		}
+		
+		return ok(Json.toJson(histogram));
+	}
 
 }

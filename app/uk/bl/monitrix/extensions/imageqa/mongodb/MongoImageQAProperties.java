03174b3a6b6340d372e3d7891fc0a4a8030de6d4
Rainer Simon <rainer.simon@ait.ac.at>
2013-03-28 14:59:25 +0100

    Image QA: basic frontend HTML + CSS

--- app/uk/bl/monitrix/extensions/imageqa/mongodb/MongoImageQAProperties.java
+++ app/uk/bl/monitrix/extensions/imageqa/mongodb/MongoImageQAProperties.java
@@ -5,11 +5,11 @@ public class MongoImageQAProperties {
 	/** Database collection name **/
 	public static final String COLLECTION_IMAGE_QA_LOG = "image_qa_results";
 	
-	public static final String FIELD_IMAGE_QA_LOG_ORIGINAL_WEB_URL  = "original_web_url";
-	public static final String FIELD_IMAGE_QA_LOG_WAYBACK_IMAGE_URL  = "wayback_image_url";
-	public static final String FIELD_IMAGE_QA_LOG_ORIGINAL_IMAGE_URL = "original_image_url";
-	public static final String FIELD_IMAGE_QA_LOG_MESSAGE = "message";
-	public static final String FIELD_IMAGE_QA_LOG_PSNR_MESSAGE = "psnr_message";
+	public static final String FIELD_IMAGE_QA_LOG_ORIGINAL_WEB_URL  = "orig_web_url";
+	public static final String FIELD_IMAGE_QA_LOG_WAYBACK_IMAGE_URL  = "wayback_image";
+	public static final String FIELD_IMAGE_QA_LOG_ORIGINAL_IMAGE_URL = "orig_image";
+	public static final String FIELD_IMAGE_QA_LOG_MESSAGE = "msg";
+	public static final String FIELD_IMAGE_QA_LOG_PSNR_MESSAGE = "psnr_msg";
 	public static final String FIELD_IMAGE_QA_LOG_LINE = "line";
 
 }
